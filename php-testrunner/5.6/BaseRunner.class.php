73b2ae1efd3587ffebdacb1951854c9a21319dbd
Robert Pritzkow <robert.pritzkow@aboutsource.net>
2016-07-08 13:33:29 +0200

    refactor testrunner image * use cli base image and optimize Dockerfile * add option to ignore warnings during phpcs * fetch composer from remote mirror

--- php-testrunner/5.6/BaseRunner.class.php
+++ php-testrunner/5.6/BaseRunner.class.php
@@ -10,28 +10,50 @@ abstract class BaseRunner {
     const DEFAULT_TIMEZONE = 'UTC';
 
     protected $_paths;
+    protected $_options;
 
-    public function __construct($paths) {
-        $paths = is_array($paths) ? $paths : array($paths);
+    public function __construct($argv) {
+
+        $paths = array_filter(array_slice($argv, 1), function($arg) {
+            return strpos($arg, '--') !== 0;
+        });
+
+        $paths = is_array($paths) ? $paths : [$paths];
 
         $this->_paths = array_map('trim', $paths);
         $this->_paths = array_unique($this->_paths);
         $this->_paths = array_map(function($path) {
            return strpos($path, '/') === 0 ? $path : "application/wp-content/{$path}";
         }, $this->_paths);
+
+        $this->_options = getopt('', $this->getOptionDefinition());
+
+        // non-value options are saved with value 'false' when they are set
+        foreach($this->getOptionDefinition() as $def) {
+            if(strpos($def, ':') === false && array_key_exists($def, $this->_options) && $this->_options[$def] === false) {
+                $this->_options[$def] = true;
+            }
+        }
     }
 
     protected function runCommand($commandTemplate, $args) {
         $exitCode = -1;
         $args = is_array($args) ? $args : array($args);
         $command = vsprintf($commandTemplate, $args);
-        echo "Running command: {$command}...\n";
+        echo "* Running command: {$command}...\n";
         system($command, $exitCode);
+        echo "* Command exited with code {$exitCode}\n";
         return $exitCode;
+    }
+
+    protected function getOption($key, $default = null)
+    {
+        return isset($this->_options[$key]) ? $this->_options[$key] : $default;
     }
 
     abstract public function execute();
     abstract protected function showBanner();
+    abstract protected function getOptionDefinition();
 
 }
 
