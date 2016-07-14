2fa296d4cf9605130d20206fcbd4557f0f236ef3
Robert Pritzkow <robert.pritzkow@aboutsource.net>
2016-07-14 18:14:32 +0200

    Added php-testrunner:7.0

---
+++ php-testrunner/7.0/README.md
@@ -0,0 +1,29 @@
+# Docker image for php-testrunner
+
+    PHP-Version: 7.0.8
+    Composer: 1.1.3
+
+Includes:
+
+* phpcs
+* phpmd
+* rulesets for WordPress, PHP-Compatibility
+* Custom about:source WordPress ruleset
+
+## Usage
+
+    run-checks <file> <folder> <file> ...
+
+This will run `phpcs` and `phpmd` with settings defined by this image.
+
+    !!! Paths without a leading slash will be prefixed with application/wp-content !!!
+
+You can of course still run `phpcs` and `phpmd` manually.
+
+### Automatic fixing
+
+    fix-issues <file> <folder> <file>...
+    
+This will run `phpcbf` with the settings defined by this image.  
+  
+    !!! Paths without a leading slash will be prefixed with application/wp-content !!!
---
+++ php-testrunner/7.0/src/BaseRunner.class.php
@@ -0,0 +1,59 @@
+<?php
+
+abstract class BaseRunner {
+
+    const EXTENSIONS = 'php,phtml';
+    const EXCLUDES = '*/vendor/*';
+    const PHPCS_STANDARDS = 'aboutsource-WordPress';
+    const PHPMD_STANDARDS = 'cleancode,codesize,design,unusedcode'; // moved to custom ruleset to disable some rule
+    const PHPMD_OUTPUT = 'text';
+    const DEFAULT_TIMEZONE = 'UTC';
+
+    protected $_paths;
+    protected $_options;
+
+    public function __construct($argv) {
+
+        $paths = array_filter(array_slice($argv, 1), function($arg) {
+            return strpos($arg, '--') !== 0;
+        });
+
+        $paths = is_array($paths) ? $paths : [$paths];
+
+        $this->_paths = array_map('trim', $paths);
+        $this->_paths = array_unique($this->_paths);
+        $this->_paths = array_map(function($path) {
+           return strpos($path, '/') === 0 ? $path : "application/wp-content/{$path}";
+        }, $this->_paths);
+
+        $this->_options = getopt('', $this->getOptionDefinition());
+
+        // non-value options are saved with value 'false' when they are set
+        foreach($this->getOptionDefinition() as $def) {
+            if(strpos($def, ':') === false && array_key_exists($def, $this->_options) && $this->_options[$def] === false) {
+                $this->_options[$def] = true;
+            }
+        }
+    }
+
+    protected function runCommand($commandTemplate, $args) {
+        $exitCode = -1;
+        $args = is_array($args) ? $args : array($args);
+        $command = vsprintf($commandTemplate, $args);
+        echo "* Running command: {$command}...\n";
+        system($command, $exitCode);
+        echo "* Command exited with code {$exitCode}\n";
+        return $exitCode;
+    }
+
+    protected function getOption($key, $default = null)
+    {
+        return isset($this->_options[$key]) ? $this->_options[$key] : $default;
+    }
+
+    abstract public function execute();
+    abstract protected function showBanner();
+    abstract protected function getOptionDefinition();
+
+}
+
